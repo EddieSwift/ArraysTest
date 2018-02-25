503a32ea1a7eb03a138b72f012b16470b019ce34
Eddie <Eddie@galchenko.local>
2018-02-25 18:21:52 +0200

    Initial Commit

---
+++ ArraysTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArraysTest
+//
+//  Created by Eddie on 25.02.2018.
+//  Copyright © 2018 Eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
